51e1a9d6ea15e822cd2ca653e46960bc071fecc0
KatlaEva <orka0002@stud.kea.dk>
2019-12-18 14:51:40 +0100

    master

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './../../src/app/components/login/login.component';
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
@@ -5,7 +6,16 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
+  login(email, password) {
+    //element(by.id('LoginForm')).click();
+    // browser.sleep(2000);
+    element(by.id('email-input')).sendKeys(email);
+    element(by.id('password-input')).sendKeys(password);
+    element(by.id('login-button')).click();
+  }
+
   getTitleText() {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
+
 }
---
+++ e2e/src/login.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { AppPage } from './app.po';
+import { browser, element, by, protractor } from 'protractor';
+
+
+describe('Login tests', ()=> {
+    let page: AppPage;
+
+    beforeEach(() => {
+        page = new AppPage();
+        page.navigateTo();
+    });
+
+    it('Should test that e2e works', ()=> {
+        browser.get('');
+        element(by.id('loginDashboard')).click();
+        expect(element(by.className('nav-link')).getText()).toEqual('Dashboard');
+        
+        
+
+        //Write code to login
+        
+
+        //more code here.
+        
+
+        expect(true).toBeTruthy();
+    })
+
+    it('Login Form should be valid', ()=> {
+        browser.get('login')
+
+        page.login('harpa@test.com', 'tester1234*');
+
+        expect(element(by.className('nav-link')).getText()).toEqual('Dashboard');
+
+    });
+
+});
